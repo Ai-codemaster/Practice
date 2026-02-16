6c35b2dcd8626c82678af1046bc46fca8a536b92
AI-Codemaster <ai5445359@gmail.com>
2026-02-16 07:31:31 -0500

    json folder add

---
+++ json/script.js
@@ -0,0 +1,16 @@
+const loadPost = () => {
+    const url = "https://jsonplaceholder.typicode.com/posts";
+
+    fetch(url)
+    .then(res => res.json())
+    .then(json => {
+        console.log(json);
+        displayPost(json);
+    })
+}
+
+const displayPost = (posts) => {
+    posts.forEach((post) => {
+        console.log(post);
+    });
+}
